f5ad19131234c3a9223a9e846e652f7dca649800
victorcesaroni <victor.cesaroni@gmail.com>
2018-04-24 15:16:41 -0300

    adicionada portas de teleporte para o final do jogo

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public bool allowOnce = true;
+    public GameObject tpObject;
+    public List<GameObject> disableList;
+    public List<GameObject> enableList;
+
+    bool locked = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (locked)
+            return;
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (var item in disableList)
+            {
+                item.SetActive(false);
+            }
+
+            foreach (var item in enableList)
+            {
+                item.SetActive(true);
+            }
+
+            other.gameObject.transform.position = tpObject.transform.position;
+
+            if (allowOnce)
+                locked = true;
+        }
+    }
+}
---
+++ Assets/Scripts/GameOverDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverDoor : MonoBehaviour {
+    public float restartDelay = 5;
+    public GameObject gameOverPanel;
+    bool restart = false;
+    float countdown = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (restart)
+        {
+            if (countdown <= 0)
+                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            countdown -= Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (restart)
+            return;
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            countdown = restartDelay;
+            restart = true;
+            gameOverPanel.SetActive(true);
+        }
+    }
+}
