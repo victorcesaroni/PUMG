8ca16b3e3beb86d853b92d89ec5ce47b175f969e
VICTOR FERNANDO CESARONI <victor.fc@puccampinas.edu.br>
2018-05-09 11:17:04 -0300

    menu funcional e interface responsiva

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtons : MonoBehaviour {
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+	}
+
+	public void Load(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+}
